2835e9421f61559188a72f8e6415484c1cde85a0
moecods <jakmesh96@gmail.com>
2024-06-25 00:48:42 +0330

    feat: Added test for binary search algorithm in descendiang array

--- binary_search/binary_search_test.go
+++ binary_search/binary_search_test.go
@@ -37,3 +37,39 @@ func TestBinarySearch(t *testing.T) {
         t.Errorf("Expected index %d, but got index %d", expected, result)
     }
 }
+
+func TestBinarySearchInDescendingArray(t *testing.T) {
+    arr := []int{91, 72, 56, 38, 23, 16, 12, 8, 4, 2}
+
+    // Test case: Target element exists in the array
+    target := 23
+    expected := 4
+    result := BinarySearchInDescendingArray(arr, target)
+    if result != expected {
+        t.Errorf("Expected index %d, but got index %d", expected, result)
+    }
+
+    // Test case: Target element does not exist in the array
+    target = 50
+    expected = -1
+    result = BinarySearchInDescendingArray(arr, target)
+    if result != expected {
+        t.Errorf("Expected index %d, but got index %d", expected, result)
+    }
+
+    // Test case: Target element is the first element in the array
+    target = 91
+    expected = 0
+    result = BinarySearchInDescendingArray(arr, target)
+    if result != expected {
+        t.Errorf("Expected index %d, but got index %d", expected, result)
+    }
+
+    // Test case: Target element is the last element in the array
+    target = 2
+    expected = 9
+    result = BinarySearchInDescendingArray(arr, target)
+    if result != expected {
+        t.Errorf("Expected index %d, but got index %d", expected, result)
+    }
+}
